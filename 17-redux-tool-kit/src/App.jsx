4e9b8b661385befe1601e0b740fc85f1f9f2633d
shreyashtalele <taleleshreyash44@gmail.com>
2026-06-28 12:33:02 +0530

    react-redux-tool-kit

---
+++ 17-redux-tool-kit/src/App.jsx
@@ -0,0 +1,12 @@
+import AddTodo from "./components/addTodo";
+import Todos from "./components/Todos";
+function App() {
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  );
+}
+
+export default App;
